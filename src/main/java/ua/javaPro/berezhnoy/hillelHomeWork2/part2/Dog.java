3884df8071638c6606b82674598d984d273a3d78
imstany <imstany@gmail.com>
2023-01-26 23:17:50 +0300

    1.1

--- src/main/java/ua/javaPro/berezhnoy/hillelHomeWork2/part2/Dog.java
+++ src/main/java/ua/javaPro/berezhnoy/hillelHomeWork2/part2/Dog.java
@@ -13,11 +13,7 @@ public class Dog extends Animals {
         if (distanceRun > maxRun) {
             System.out.println(getName() + " " + "Не пробежал " + distanceRun + "м" + ",устал ");
         } else {
-
             System.out.println(getName() + " " + "Пробежал " + distanceRun + "м");
-
-            runStr = name + " " + "пробежал " + distanceRun + "м";
-
         }
     }
 
@@ -26,27 +22,7 @@ public class Dog extends Animals {
         if (distance > maxSwim) {
             System.out.println(getName() + " " + "Не проплыл " + distance + "м" + " слишком большое растояние ");
         } else {
-
             System.out.println(getName() + " " + "Проплыл " + distance + "м");
-
-            swimStr = name + " " + "проплыл " + distance + "м";
-        }
-    }
-
-    @Override
-    public String toString() {
-        if (swim) {
-            return "Dog{" +
-                    "Имя: " + name + ", " +
-                    runStr + ", " +
-                    swimStr +
-                    '}';
-        } else {
-            return "Dog{" +
-                    "Имя: " + name + ", " +
-                    runStr +
-                    '}';
-
         }
     }
 }
