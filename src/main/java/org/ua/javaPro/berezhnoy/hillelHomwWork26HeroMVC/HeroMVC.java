1100f970e9ee0c88ab6ea37b55a65ae1f653998a
Stany1987 <imstany@gmail.com>
2023-05-23 10:42:43 +0300

    hillelHomeWork26MVC

---
+++ src/main/java/org/ua/javaPro/berezhnoy/hillelHomwWork26HeroMVC/HeroMVC.java
@@ -0,0 +1,12 @@
+package org.ua.javaPro.berezhnoy.hillelHomwWork26HeroMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroMVC {
+    public static void main(String[] args) {
+        SpringApplication.run(HeroMVC.class, args);
+    }
+
+}
---
+++ src/main/java/org/ua/javaPro/berezhnoy/hillelHomwWork26HeroMVC/HeroNotFoundException.java
@@ -0,0 +1,7 @@
+package org.ua.javaPro.berezhnoy.hillelHomwWork26HeroMVC;
+
+public class HeroNotFoundException extends RuntimeException {
+    public HeroNotFoundException(String message) {
+        super(message);
+    }
+}
