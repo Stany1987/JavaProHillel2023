f69e3679fa93380d0008726d54cf7397257bafa4
Stany1987 <imstany@gmail.com>
2023-06-08 13:55:37 +0300

    hillelHomeWork29IntegrationTest

--- src/main/java/org/ua/javaPro/berezhnoy/bank/hillelHomeWork27DataJPA/DataConfiguration.java
+++ src/main/java/org/ua/javaPro/berezhnoy/bank/hillelHomeWork27DataJPA/DataConfiguration.java
@@ -1,37 +1,12 @@
 package org.ua.javaPro.berezhnoy.bank.hillelHomeWork27DataJPA;
 
-import org.postgresql.ds.PGSimpleDataSource;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
+
+
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-
-import javax.sql.DataSource;
-
+@ComponentScan("org.ua.javaPro.berezhnoy.hillelHomeWork24SpringCore")
 @Configuration
-@EnableConfigurationProperties
 public class DataConfiguration {
-    @Value("${spring.second-datasource.url}")
-    private String secondUrl;
 
-    @Value("${spring.datasource.databasename}")
-    private String secondDatabaseName;
-
-    @Value("${spring.second-datasource.username}")
-    private String secondUserNAme;
-
-    @Value("${spring.second-datasource.password}")
-    private String secondPassword;
-
-    @Bean
-    public DataSource dataSource() {
-        var ds = new PGSimpleDataSource();
-        ds.setUrl(secondUrl);
-        ds.setDatabaseName(secondDatabaseName);
-        ds.setUser(secondUserNAme);
-        ds.setPassword(secondPassword);
-        return ds;
-    }
 }
